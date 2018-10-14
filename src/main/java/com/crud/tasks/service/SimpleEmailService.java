3299e1757fb9bb18c69f4c340e91e455836043d7
pawelslupski <pawelslupski.go@gmail.com>
2018-10-14 12:32:59 +0200

    The latest update

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -29,9 +29,7 @@ public class SimpleEmailService {
     private SimpleMailMessage createMailMessage(final Mail mail) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
-        if(mail.getToCc() != null) {
-            mailMessage.setCc(mail.getToCc());
-        }
+        if(mail.getToCc() != null) mailMessage.setCc(mail.getToCc());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
         return mailMessage;
