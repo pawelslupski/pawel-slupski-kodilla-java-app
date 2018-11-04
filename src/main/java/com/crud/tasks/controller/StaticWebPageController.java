063a43022d36b11b0bf8f203ac0c1f431f87aa98
pawelslupski <pawelslupski.go@gmail.com>
2018-11-04 15:01:30 +0100

    24.1.MathOperations_Thymeleaf

---
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -0,0 +1,24 @@
+package com.crud.tasks.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class StaticWebPageController {
+
+    @RequestMapping("/")
+    public String index(Map<String, Object> model) {
+        model.put("variable", "My Thymeleaf variable");
+        model.put("multiply", "*");
+        model.put("adding", "+");
+        model.put("subtraction", "-");
+        model.put("equals", "=");
+        model.put("space", " ");
+        model.put("one", 1);
+        model.put("two", 2);
+        model.put("three", 3);
+        return "index";
+    }
+}
